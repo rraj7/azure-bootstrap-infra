32a419cd38f6907bc23135c20a9281c9cb0e1668
Rishi Raj <rishiraj3107@gmail.com>
2025-04-22 00:26:11 -0500

    Bug fix branch

--- environments/DEV/main.tf
+++ environments/DEV/main.tf
@@ -1,21 +1,26 @@
-# Fetch the object ID of GitHub Actions workload identity
-locals {
-  resource_group_name = "rg-bootstrap-dev"
-}
-data "azuread_service_principal" "gh_oidc" {
-  display_name = "terraform-gh-actions-app"
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.env_name}-bootstrap"
+  location = var.location
 }
 
-# Create Azure Key Vault via reusable module
-module "keyvault" {
-  source                     = "../../modules/key_vault"
-  name                       = "kv-bootstrap-dev"
-  env_name                   = "dev"
-  location                   = "EastUs"
-  resource_group_name        = local.resource_group_name
-  tenant_id                  = var.tenant_id
-  object_id                  = data.azuread_service_principal.gh_oidc.object_id
-  sku_name                   = "standard"
-  soft_delete_retention_days = 7
-  purge_protection_enabled   = true
+resource "azurerm_key_vault" "kv" {
+  name                        = "kv-${var.env_name}-bootstrap"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = true
+
+  access_policy {
+    tenant_id = var.tenant_id
+    object_id = var.object_id
+
+    secret_permissions = ["Get", "List", "Set", "Delete"]
+    key_permissions    = ["Get", "List"]
+  }
+
+  tags = {
+    environment = var.env_name
+  }
 }
--- environments/DEV/variables.tf
+++ environments/DEV/variables.tf
@@ -1,40 +1,19 @@
-variable "name" {
-  description = "Name of the Key Vault"
+variable "env_name" {
+  description = "Environment name"
   type        = string
 }
 
 variable "location" {
   description = "Azure region"
   type        = string
-  default     = "Central US"
-}
-
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
 }
 
 variable "tenant_id" {
-  description = "Azure Tenant ID"
+  description = "Azure AD Tenant ID"
   type        = string
 }
 
 variable "object_id" {
-  description = "Object ID for Key Vault access policy"
-  type        = string
-}
-
-variable "sku_name" {
-  description = "SKU for the Key Vault"
-  type        = string
-}
-
-variable "env_name" {
-  description = "Environment name"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "Azure subscription ID"
+  description = "OIDC Principal Object ID"
   type        = string
 }
